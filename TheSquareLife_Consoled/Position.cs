2daae2b60da7ee90cccb8ea200877971e9a8da9d
GrindelfP <grindelf.perlomutrovij@gmail.com>
2022-05-20 09:41:50 +0300

    test commit

--- TheSquareLife_Consoled/Position.cs
+++ TheSquareLife_Consoled/Position.cs
@@ -3,18 +3,25 @@ namespace TheSquareLife_Consoled;
 internal class Position
 {
     public HashSet<Coordinate> Coordinates;
-    internal protected Position(HashSet<Coordinate> coordinates)
+
+    public List<Coordinate> UpperCoordinates()
+    {
+        var min = int.MaxValue;
+        Coordinates.ForEach(it =>
+        {
+            min = it.Y < min ? it.Y : min;
+        });
+        var resultingCoordinates = Coordinates.Where(it => it.Y == min); 
+        foreach (var resultingCoordinate in resultingCoordinates)
+        {
+            resultingCoordinate.ShiftUp();
+        }
+
+        return (List<Coordinate>)resultingCoordinates; // May not work this way. Further investigation required.
+    }
+    
+    protected internal Position(HashSet<Coordinate> coordinates)
     {
         Coordinates = coordinates;
-    }
-}
-struct EntityPosition
-{
-    Position Position;
-    Color Color;
-    public EntityPosition(Position position, Color color)
-    {
-        Position = position;
-        Color = color;
     }
 }
