6558b899946ff5e1260a282dbdb89e82bf620ad0
GrindelfP <grindelf.perlomutrovij@gmail.com>
2022-05-24 19:02:47 +0300

    change method modifiers in IEntitiesRelationships.cs

--- TheSquareLife_Consoled/Population/IEntitiesRelationships.cs
+++ TheSquareLife_Consoled/Population/IEntitiesRelationships.cs
@@ -2,7 +2,7 @@ namespace TheSquareLife_Consoled;
 
 internal interface IEntitiesRelationships
 {
-    public abstract HashSet<Coordinate> CommonCoordinates(List<Entity> overlappingAliens);
-    public abstract HashSet<Coordinate> CommonCoordinates(Entity overlappingEntity);
-    public abstract bool OverlapWithAlien(Entity otherEntity);
+    public HashSet<Coordinate> CommonCoordinates(List<Entity> overlappingAliens);
+    public HashSet<Coordinate> CommonCoordinates(Entity overlappingEntity);
+    public bool OverlapWithAlien(Entity otherEntity);
 }
